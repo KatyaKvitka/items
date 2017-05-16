e326db0cb0dfa931f94525a7c318bec5464f90a2
Katerina Kvitka <kvito4ka1@gmail.com>
2017-05-16 21:47:50 +0300

    fix servise2

--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -3,14 +3,13 @@ import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
+
 export class ItemsService{
 	constructor(private http: Http){
 		console.log('ItemsService Initialize...');
 	}
 	getItems(){
+		console.log('ItemsService Initialize2...');
 		return this.http.get("../../assets/items.json").map(res => res.json());
 	}
-
-	
-
 }
