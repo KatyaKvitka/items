16e0375f9746a9453e6dbf10d50d4c570db52edd
Katerina Kvitka <kvito4ka1@gmail.com>
2017-05-16 20:41:42 +0300

    add project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './components/navigation.component';
+import { ItemsComponent } from './components/items.component';
+import { MessagesComponent } from './components/messages.component';
+
+import { MaterialModule,MdButtonModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { routing } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ItemsComponent,
+    MessagesComponent
   ],
+  
   imports: [
+    routing,
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    MdButtonModule,  
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
